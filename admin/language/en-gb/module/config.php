3571170e9fc895b2f8ca604815e53937a7f879a9
Valdeir S <valdeirpsr@hotmail.com.br>
2022-08-20 15:09:00 -0300

    feat: adiciona campos da aba de configurações gerais
    Nesta guia, o usuário poderá escolher se o módulo estará ou não
    habilitado para o uso

--- admin/language/en-gb/module/config.php
+++ admin/language/en-gb/module/config.php
@@ -6,3 +6,26 @@ $_['heading_title'] = 'Avise-me quando disponível';
 // Text
 $_['text_dashboard']  = 'Dashboard';
 $_['text_extensions'] = 'Extensions';
+$_['text_sqs_info'] = 'Para outras formas de autenticação, entre em contato com <a href="https://valdeir.dev/" target="_blank">https://valdeir.dev/</a>';
+$_['text_ses_info'] = 'Entre em contato com <a href="https://valdeir.dev/" target="_blank">https://valdeir.dev/</a>';
+$_['text_redis_info'] = 'Entre em contato com <a href="https://valdeir.dev/" target="_blank">https://valdeir.dev/</a>';
+
+// Entry
+$_['entry_status'] = 'Status';
+$_['entry_notification_type'] = 'Sistema de Notificação';
+$_['entry_smtp_hostname'] = 'Host';
+$_['entry_smtp_username'] = 'Usuário';
+$_['entry_smtp_password'] = 'Senha';
+$_['entry_smtp_port'] = 'Porta';
+$_['entry_smtp_timeout'] = 'Timeout';
+$_['entry_sqs_access_key'] = 'Access Key';
+$_['entry_sqs_secret_key'] = 'Secret Key';
+$_['entry_sqs_region'] = 'Região';
+$_['entry_sqs_queue_url'] = 'Queue URL';
+$_['entry_sqs_total_emails'] = 'Total de e-mail por fila';
+
+// Help
+$_['help_smtp_hostname'] = 'Add \'tls://\' or \'ssl://\' prefix if security connection is required. (e.g. tls://smtp.gmail.com, ssl://smtp.gmail.com).';
+$_['help_smtp_password'] = 'For Gmail you might need to setup an application specific password here: <a href="https://security.google.com/settings/security/apppasswords" target="_blank">https://security.google.com/settings/security/apppasswords</a>.';
+$_['help_sqs_region'] = 'Access <a href="https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html">Region availability</a> to see the list of regions';
+$_['help_sqs_total_emails'] = 'Informe o total de e-mail que deverão ser enviados por fila';

23e4afaa3f31b65ea0b02424e425cae854a27af6
Valdeir S <valdeirpsr@hotmail.com.br>
2022-08-23 23:39:27 -0300

    feat: adiciona model
    No model, registra os métodos para criar e remover as tabelas após a
    instalação e remoção da extensão, respectivamente.

---
+++ admin/model/module/config.php
@@ -0,0 +1,27 @@
+<?php
+namespace OpenCart\Admin\Model\Extension\LetMeKnowWheItsAvailable\Module;
+
+class Config extends \OpenCart\System\Engine\Model
+{
+    public function createTables(): void
+    {
+        $this->db->query('
+            CREATE TABLE `' . DB_PREFIX . 'let_me_know` (
+                `id` INT AUTO_INCREMENT PRIMARY KEY,
+                `customer_name` VARCHAR(64) NOT NULL,
+                `customer_email` VARCHAR(255) NOT NULL,
+                `product_id` INT(11) NOT NULL,
+                `product_name` VARCHAR(255) NOT NULL,
+                `language_id` INT(11) NOT NULL,
+                `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
+                `concluded_at` DATETIME NULL,
+                `modified_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+            );
+        ');
+    }
+
+    public function dropTables(): void
+    {
+        $this->db->query('DROP TABLE `' . DB_PREFIX . 'let_me_know`');
+    }
+}

d2ac3eedfb5df3e425ea10284eec34fe8db19587
Valdeir S <valdeirpsr@hotmail.com.br>
2022-08-24 18:15:10 -0300

    refactor: altera colunas ao criar tabela
    Remover product_name e adiciona custom_fields

--- admin/model/module/config.php
+++ admin/model/module/config.php
@@ -11,8 +11,8 @@ class Config extends \OpenCart\System\Engine\Model
                 `customer_name` VARCHAR(64) NOT NULL,
                 `customer_email` VARCHAR(255) NOT NULL,
                 `product_id` INT(11) NOT NULL,
-                `product_name` VARCHAR(255) NOT NULL,
                 `language_id` INT(11) NOT NULL,
+                `custom_fields` JSON NOT NULL,
                 `created_at` DATETIME DEFAULT CURRENT_TIMESTAMP,
                 `concluded_at` DATETIME NULL,
                 `modified_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP

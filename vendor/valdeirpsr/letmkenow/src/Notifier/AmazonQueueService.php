08d452619982fa357c08a30bc08dca0ee6aa35ef
Valdeir S <valdeirpsr@hotmail.com.br>
2022-09-02 18:01:22 -0300

    refactor: remove try..catch do notificador SQS
    O tratamento será feito no controller

--- vendor/valdeirpsr/letmkenow/src/Notifier/AmazonQueueService.php
+++ vendor/valdeirpsr/letmkenow/src/Notifier/AmazonQueueService.php
@@ -4,14 +4,6 @@ namespace ValdeirPsr\Letmeknow\Notifier;
 use ValdeirPsr\Letmeknow\NotifierInterface;
 use ValdeirPsr\Letmeknow\Logger;
 use Aws\Sqs\SqsClient;
-use Aws\Sqs\Exception\SqsException\{
-    TooManyEntriesInBatchRequest,
-    EmptyBatchRequest,
-    BatchEntryIdsNotDistinct,
-    BatchRequestTooLong,
-    InvalidBatchEntryId,
-    UnsupportedOperation
-};
 use Opencart\System\Engine\Config;
 
 class AmazonQueueService implements NotifierInterface
@@ -60,36 +52,24 @@ class AmazonQueueService implements NotifierInterface
         $confirmed = [];
         $unconfirmed = [];
 
-        try {
-            foreach ($registers as $key => $register) {
-                $entries[] = [
-                    'Id' => $key,
-                    'MessageBody' => json_encode($register)
-                ];
+        foreach ($registers as $key => $register) {
+            $entries[] = [
+                'Id' => $key,
+                'MessageBody' => json_encode($register)
+            ];
 
-                $unconfirmed[] = $register['customer_email'];
+            $unconfirmed[] = $register['customer_email'];
 
-                if (count($entries) % 1 === 0) {
-                    $this->sqs->sendMessageBatch([
-                        'Entries' => $entries,
-                        'QueueUrl' => $this->queueUrl
-                    ]);
+            if (count($entries) % 1 === 0) {
+                $this->sqs->sendMessageBatch([
+                    'Entries' => $entries,
+                    'QueueUrl' => $this->queueUrl
+                ]);
 
-                    $confirmed = array_merge($confirmed, $unconfirmed);
-                    $entries = [];
-                    $unconfirmed = [];
-                }
+                $confirmed = array_merge($confirmed, $unconfirmed);
+                $entries = [];
+                $unconfirmed = [];
             }
-        } catch (TooManyEntriesInBatchRequest | BatchEntryIdsNotDistinct | BatchRequestTooLong $e) {
-            Logger::error($e->getMessage(), $e);
-        } catch (EmptyBatchRequest $e) {
-            Logger::notice($e->getMessage(), $e);
-        } catch (InvalidBatchEntryId $e) {
-            Logger::warning($e->getMessage(), $e);
-        } catch (UnsupportedOperation $e) {
-            Logger::critical($e->getMessage(), $e);
-        } catch (\Exception $e) {
-            Logger::error($e->getMessage(), $e);
         }
 
         return $confirmed;

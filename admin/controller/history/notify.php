cac70f286ba9fcfcc756b3a1cae136d7c2b36a14
Valdeir S <valdeirpsr@hotmail.com.br>
2022-09-03 22:08:39 -0300

    refactor: adiciona o try..catch no controller de notificação
    Em caso de erro, exibe uma mensagem para o usuário

--- admin/controller/history/notify.php
+++ admin/controller/history/notify.php
@@ -21,7 +21,7 @@ class Notify extends \OpenCart\System\Engine\Controller
 {
     const EXTENSION_PREFIX = 'module_letmeknow_';
     const EXTENSION_CODE = 'LetMeKnowWheItsAvailable';
-    const EXTENSION_PATH_MODULE = 'extension/' . self::EXTENSION_CODE . '/history';
+    const EXTENSION_PATH_HISTORY = 'extension/' . self::EXTENSION_CODE . '/history';
     const EXTENSION_MODEL_NOTIFIER = 'model_extension_' . self::EXTENSION_CODE . '_history_notifier';
 
     /**
@@ -33,12 +33,14 @@ class Notify extends \OpenCart\System\Engine\Controller
             require_once DIR_EXTENSION . self::EXTENSION_CODE . '/vendor/autoload.php';
         }
 
+        $this->load->language(self::EXTENSION_PATH_HISTORY . '/notify');
+
         define('LETMEKNOW_LOG', DIR_EXTENSION . self::EXTENSION_CODE . '/system/storage/logs');
 
         $productId = filter_input(INPUT_POST, 'product_id', FILTER_SANITIZE_NUMBER_INT);
         $email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 
-        $this->load->model(self::EXTENSION_PATH_MODULE . '/notifier');
+        $this->load->model(self::EXTENSION_PATH_HISTORY . '/notifier');
 
         $emails = $this->{self::EXTENSION_MODEL_NOTIFIER}->getEmails($productId, $email);
 
@@ -53,11 +55,34 @@ class Notify extends \OpenCart\System\Engine\Controller
         }
 
         if (!$json) {
-            $sender = new \ValdeirPsr\Letmeknow\Sender($mail);
-            $sender->setRegisters($emails);
-            $confirmed = $sender->send($emails);
+            try {
+                $sender = new \ValdeirPsr\Letmeknow\Sender($mail);
+                $sender->setRegisters($emails);
+                $confirmed = $sender->send($emails);
 
-            $this->{self::EXTENSION_MODEL_NOTIFIER}->confirm($productId, $confirmed);
+                $this->{self::EXTENSION_MODEL_NOTIFIER}->confirm($productId, $confirmed);
+            } catch (TooManyEntriesInBatchRequest $e) {
+                Logger::error($e->getMessage(), ['Obj' => $e]);
+                $json['error'] = $this->language->get('error_too_many_entries_in_batch_request');
+            } catch (EmptyBatchRequest $e) {
+                Logger::warning($e->getMessage(), ['Obj' => $e]);
+                $json['error'] = $this->language->get('error_empty_batch_request');
+            } catch (BatchEntryIdsNotDistinct $e) {
+                Logger::error($e->getMessage(), ['Obj' => $e]);
+                $json['error'] = $this->language->get('error_batch_entry_ids_not_distinct');
+            } catch (BatchRequestTooLong $e) {
+                Logger::warning($e->getMessage(), ['Obj' => $e]);
+                $json['error'] = $this->language->get('error_batch_request_too_long');
+            } catch (InvalidBatchEntryId $e) {
+                Logger::error($e->getMessage(), ['Obj' => $e]);
+                $json['error'] = $this->language->get('error_invalid_batch_entry_id');
+            } catch (UnsupportedOperation $e) {
+                Logger::error($e->getMessage(), ['Obj' => $e]);
+                $json['error'] = $this->language->get('error_unsupported_operation');
+            } catch (\Exception $e) {
+                Logger::error($e->getMessage(), ['Obj' => $e]);
+                $json['error'] = $this->language->get('error_unknow');
+            }
         }
 
         $this->response->addHeader('Content-Type: application/json');
@@ -85,7 +110,7 @@ class Notify extends \OpenCart\System\Engine\Controller
         } else if ($this->config->get('config_mail_engine') === 'mail') {
             return new Mail($this->config);
         } else {
-            Logger::warning('Engine not defined in OpenCart Settings');
+            Logger::warning($this->language->get('error_none_engine_mail'));
         }
 
         return null;
